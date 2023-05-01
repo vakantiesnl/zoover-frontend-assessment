6ed3259113a9a5a7c627df95bb40f5a42520539c
nikbasm <n.basmatzidis@student.vu.nl>
2023-05-01 02:17:06 +0200

    Enhanced the UserReview component and the css(made the code more readable, added css classes)

--- client/src/Components/UserReview/UserReview.js
+++ client/src/Components/UserReview/UserReview.js
@@ -18,23 +18,28 @@ const UserReview = ({review}) => {
     return(
         <div>
             <div className="reviews">
-                
-                <div className="user-name">
-                    Added by <p> {review.user} </p> <CountryFlag countryCode={review.locale}/> On {formatDate(review.entryDate, "entryDate")}
-                </div>
-                
-                <div className="review-title"> {review.titles[review.locale]} </div>
+                <div className="general-review">
+                    <div>
+                        <div className="user-name">
+                            Added by 
+                            <p className="bold-text"> {review.user} </p> 
+                            <CountryFlag countryCode={review.locale}/> On {formatDate(review.entryDate, "entryDate")}
+                        </div>
+                        <div className="review-title"> {review.titles[review.locale]} </div>
+                        <div className="review-text"> {review.texts[review.locale]} </div>
+                    </div>
 
-                <div className="review-text"> {review.texts[review.locale]} </div>
-
-                <div className="general"> General review ({review.ratings.general.general}/10) </div>
-                
-                <StarRating value ={review.ratings.general.general}/>
-
-                <div className="info"> 
-                    <div>Information about the trip </div>
-                    <div> Date: {formatDate(review.travelDate, "travelDate")}</div>
-                    <div>TravelledWith {review.traveledWith}</div>
+                    <div className="general-info">
+                        <div className="general-stars"> 
+                            General ({review.ratings.general.general}/10)
+                            <StarRating value ={review.ratings.general.general}/> 
+                        </div>
+                        <div className="info">
+                            <div> About the trip </div>
+                            <div> Date: <span className="bold-text"> {formatDate(review.travelDate, "travelDate")}</span></div>
+                            <div> With: <span className="bold-text"> {review.traveledWith}</span></div>
+                        </div>
+                    </div>   
                 </div> 
 
                 <h4>Ratings of aspects</h4>
